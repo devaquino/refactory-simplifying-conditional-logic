f4b47ee526db8bba38df8aec0ad7ee73415ae560
gabriel <gabraquino@gmail.com>
2019-04-09 20:53:20 -0300

    Add Consolidate COnditional Expression example

---
+++ consolidateConditionalExpression.js
@@ -0,0 +1,33 @@
+// Needs Refactory
+{
+  if(anEmployee.seniority < 2) return 0;
+  if(anEmployee.monthsDisabled > 12) return 0;
+  if(anEmployee.isPartTime) return 0;
+}
+
+// Done
+
+if(isNotEligableForDisability()) return 0;
+
+function isNotEligableForDisability() {
+  return ((anEmployee.seniority < 2)
+          (anEmployee.monthsDisabled > 12)
+          (anEmployee.isPartTime))
+         );
+}
+
+// #######
+
+// Needs Refactory
+{
+  if(anEmployee.onVacation)
+    if(anEmployee.seniority > 10)
+      return 1;
+  return 0.5;
+}
+
+
+// Done
+if((anEmployee.onVacation) && (anEmployee.seniority > 10)) return 1;
+
+return 0.5
